d424684a3413a2e807413abe7edb560ea465e417
Abdullahi Addow <jiincade@gmail.com>
2021-08-24 11:12:27 +0300

    settings page design

--- lib/ui/pages/settings_page.dart
+++ lib/ui/pages/settings_page.dart
@@ -10,51 +10,95 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Settings',
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        automaticallyImplyLeading: false,
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            LocaleKeys.app_title,
-            style: Theme.of(context).textTheme.headline6!.copyWith(
-                  color: Theme.of(context).colorScheme.onSecondary,
-                ),
-          ).tr(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  await context.setLocale(Locale('en'));
-                },
-                child: Text(
-                  'English',
-                  style: Theme.of(context)
-                      .textTheme
-                      .button!
-                      .copyWith(color: Colors.white),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: Theme.of(context).primaryColor,
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  await context.setLocale(Locale('ar'));
-                },
-                child: Text(
-                  'العربية',
-                  style: Theme.of(context)
-                      .textTheme
-                      .button!
-                      .copyWith(color: Colors.white),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: Theme.of(context).primaryColor,
-                ),
-              )
-            ],
+          CustomDivider(height: 10, child: SizedBox()),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              'Become a member',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1!
+                  .copyWith(color: Colors.green),
+            ),
           ),
-          BlocBuilder<ThemeCubit, bool>(
+          CustomDivider(
+            height: 40,
+            child: Text('Configure Medium'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 5, bottom: 8),
+            child: Text(
+              'Customize your interests',
+              style:
+                  Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 15),
+            ),
+          ),
+          CustomDivider(height: 1, child: SizedBox()),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Dark mode',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1!
+                      .copyWith(fontSize: 15),
+                ),
+                Text(
+                  'System default',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1!
+                      .copyWith(color: Colors.green),
+                ),
+              ],
+            ),
+          ),
+          CustomDivider(height: 1, child: SizedBox()),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomDivider extends StatelessWidget {
+  const CustomDivider({
+    Key? key,
+    required this.height,
+    required this.child,
+  }) : super(key: key);
+  final double height;
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).dividerColor,
+      height: height,
+      child: child,
+      width: double.infinity,
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.only(left: 10),
+    );
+  }
+}
+/*
+
+  BlocBuilder<ThemeCubit, bool>(
             builder: (context, isDark) {
               return Switch.adaptive(
                 value: isDark,
@@ -62,8 +106,7 @@ class SettingsPage extends StatelessWidget {
               );
             },
           )
-        ],
-      ),
-    );
-  }
-}
+
+
+          await context.setLocale(Locale('en'));
+*/
