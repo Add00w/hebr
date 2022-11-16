5bebb173eb57b974601a5c80dd1e2da15471f7a6
Abdullahi Addow <jiincade@gmail.com>
2022-11-16 13:25:26 +0300

    made the app text fields unfocusable by tapping outside.

--- lib/hebr_app.dart
+++ lib/hebr_app.dart
@@ -46,9 +46,34 @@ class HebrApp extends StatelessWidget {
                 );
               },
             ),
+            builder: (_, child) => _Unfocus(child: child!),
           );
         },
       ),
+    );
+  }
+}
+
+/// A widget that unfocus everything when tapped.
+///
+/// This implements the "Unfocus when tapping in empty space" behavior for the
+/// entire application.
+class _Unfocus extends StatelessWidget {
+  const _Unfocus({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: child,
     );
   }
 }
