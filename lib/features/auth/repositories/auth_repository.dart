61e4e9c75f66d9c8c3bb7c96fd0173393832d82d
Abdullahi Addow <jiincade@gmail.com>
2023-01-01 16:39:53 +0300

    remove the repository abstract.

--- lib/features/auth/repositories/auth_repository.dart
+++ lib/features/auth/repositories/auth_repository.dart
@@ -1,15 +1,32 @@
-abstract class AuthRepository {
-  Future<dynamic> signInWithEmailAndPassword({
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepository {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  Future<void> signInWithEmailAndPassword({
     required String username,
     required String password,
     required bool isRemember,
-  });
-  Future<void> signout();
-  Future<dynamic> createUserWithEmailAndPassword({
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+      email: username,
+      password: password,
+    );
+  }
+
+  Future<void> createUserWithEmailAndPassword({
     required String username,
     required String email,
     required String password,
     required String confirmPassword,
     required bool agreePolicy,
-  });
+  }) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signout() async {
+    await _firebaseAuth.signOut();
+  }
 }
