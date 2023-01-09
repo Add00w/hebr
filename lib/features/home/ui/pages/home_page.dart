a8b32e98d07c5dc9b2a0b081595c36b701297121
Abdullahi Addow <jiincade@gmail.com>
2023-01-09 16:44:09 +0300

    make the screen scrollable.

--- lib/features/home/ui/pages/home_page.dart
+++ lib/features/home/ui/pages/home_page.dart
@@ -16,81 +16,86 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15, right: 15, top: 5, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Following',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  Badge(
-                    badgeColor: Colors.green,
-                    position: BadgePosition.topEnd(top: 0, end: 0),
-                    showBadge: false,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Following',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Badge(
+                        badgeColor: Colors.green,
+                        position: BadgePosition.topEnd(top: 0, end: 0),
+                        showBadge: false,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            border: Border.all(
                               color: Theme.of(context)
                                   .colorScheme
-                                  .primaryContainer),
-                          shape: BoxShape.circle),
-                      padding: const EdgeInsets.all(5),
-                      child: const Icon(
-                        Icons.notifications_none,
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                                  .primaryContainer,
+                            ),
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Icon(
+                              Icons.notifications_none,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    itemCount: _followingTopics.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Chip(
+                          padding: EdgeInsets.zero,
+                          label: Text(
+                            _followingTopics[index],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 4),
+                myDivider(),
+                const SizedBox(height: 4),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    'TOP STORIES',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                Flexible(
+                  child: ListView.separated(
+                    physics: const ScrollPhysics(),
+                    itemBuilder: (context, index) => const ArticleWidget(),
+                    separatorBuilder: (context, index) => myDivider(),
+                    itemCount: 5,
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _followingTopics.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(left: 15, right: 5),
-                    child: Chip(
-                      padding: EdgeInsets.zero,
-                      labelPadding: const EdgeInsets.only(
-                        right: 8,
-                        left: 8,
-                      ),
-                      label: Text(
-                        _followingTopics[index],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 4),
-            myDivider(),
-            const SizedBox(height: 4),
-            Padding(
-              padding: const EdgeInsets.only(left: 15),
-              child: Text(
-                'TOP STORIES',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ),
-            Expanded(
-              flex: 11,
-              child: ListView.separated(
-                itemBuilder: (context, index) => const ArticleWidget(),
-                separatorBuilder: (context, index) => myDivider(),
-                itemCount: 5,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
